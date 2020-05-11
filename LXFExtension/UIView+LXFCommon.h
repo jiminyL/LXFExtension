65be3a9af06ac244d52e2905f101643d403465e1
jiminyL <jaewon_89@me.com>
2020-05-11 09:20:41 +0800

    增加UIView渐变

--- LXFExtension/UIView+LXFCommon.h
+++ LXFExtension/UIView+LXFCommon.h
@@ -14,6 +14,8 @@ NS_ASSUME_NONNULL_BEGIN
 
 - (void)rotaion180;
 - (void)rotationToOrg;
+///增加渐变
+- (void)addGradientWithColors:(NSArray<UIColor *> *)colors horizontal:(BOOL)isHorizontal;
 
 @end
 
--- LXFExtension/UIView+LXFCommon.m
+++ LXFExtension/UIView+LXFCommon.m
@@ -22,4 +22,31 @@
     }];
 }
 
+///增加渐变
+- (void)addGradientWithColors:(NSArray<UIColor *> *)colors horizontal:(BOOL)isHorizontal{
+    CAGradientLayer *layer = [CAGradientLayer layer];
+    layer.startPoint = CGPointMake(0, 0);
+    layer.endPoint = CGPointMake(isHorizontal ? 1:0, isHorizontal ? 0 : 1);
+    NSMutableArray *cgColors = [[NSMutableArray alloc] initWithCapacity:colors.count];
+    NSMutableArray *locations = [[NSMutableArray alloc] initWithCapacity:colors.count];
+    CGFloat space = 1.f / (colors.count - 1);
+    CGFloat offset = 0.f;
+    for (int i=0; i<colors.count; i++) {
+        UIColor *c = colors[i];
+        [cgColors addObject:(__bridge id)c.CGColor];
+        if (i==0) {
+            [locations addObject:@0];
+        }else if (i==colors.count - 1) {
+            [locations addObject:@1];
+        }else {
+            [locations addObject:[NSNumber numberWithFloat:offset]];
+        }
+        offset += space;
+    }
+    layer.colors = cgColors;
+    layer.locations = locations;
+    layer.frame = self.layer.bounds;
+    [self.layer insertSublayer:layer atIndex:0];
+}
+
 @end
